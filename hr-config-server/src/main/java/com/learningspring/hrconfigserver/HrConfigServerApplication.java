77aa4197ac0537b611ae673a3afe94b213ba7cd5
leandro <lgomesb@gmail.com>
2021-03-27 05:28:54 -0300

    Private Git repository

--- hr-config-server/src/main/java/com/learningspring/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/learningspring/hrconfigserver/HrConfigServerApplication.java
@@ -1,15 +1,24 @@
 package com.learningspring.hrconfigserver;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication {
+public class HrConfigServerApplication  implements CommandLineRunner {
 
+//	@Value("${spring.cloud.config.server.git.username}")
+//	private String username;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+//		System.out.println("USER_NAME = " + username);
 	}
 
 }
